a5659e4d346067692fbbacc154cdfae344eacdfb
Andy Nicholson <andrew@anicholson.net>
2023-06-14 13:19:54 +1000

    Adding typespecs for structs

--- lib/models/game.ex
+++ lib/models/game.ex
@@ -1,8 +1,14 @@
 require Logger
 
 defmodule Azul.Models.Game do
+  @type t :: %__MODULE__{
+          players: [Azul.Models.Player],
+          current_player: Azul.Models.Player.t() | nil,
+          scores: %{Azul.Models.Player.t() => integer()}
+        }
   defstruct players: [], current_player: nil, scores: %{}
 
+  @spec new([Azul.Models.Player], [{:start_player, Azul.Models.Player.t() | nil}, ...]) :: Azul.Models.Game.t()
   def new(players, start_player: s) do
     scores = Enum.map(players, fn player -> {player, 0} end) |> Enum.into(%{})
     current_player = if s == nil do
@@ -14,6 +20,10 @@ defmodule Azul.Models.Game do
     %Azul.Models.Game{players: players, current_player: current_player, scores: scores}
   end
 
+  @spec score_for(
+          Azul.Models.Game.t(),
+          Azul.Models.Player.t()
+        ) :: integer() | nil
   def score_for(%Azul.Models.Game{scores: scores}, player) do
     scores[player]
   end
--- lib/models/player.ex
+++ lib/models/player.ex
@@ -1,3 +1,6 @@
 defmodule Azul.Models.Player do
+  @type t :: %__MODULE__{
+          name: String.t()
+        }
   defstruct name: ""
 end
